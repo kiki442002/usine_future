63e20094242ef8bd7d5b894411297d39a3f2bc94
Killian <picot.killian@outlook.fr>
2024-05-22 16:43:03 +0200

    maj ota

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include "main.h"
 #include "Interrupt.h"
 #include "WiFiManager.h"
+#include "ArduinoOTA.h"
 
 DFRobot_Touch_GT911 touch;
 DFRobot_ILI9488_320x480_HW_SPI screen(/*dc=*/TFT_DC, /*cs=*/TFT_CS, /*rst=*/TFT_RST);
@@ -29,10 +30,49 @@ void setup()
     }
     Serial.println("Wifi connecter");
 
+    /*MAJ OTA*/
+    ArduinoOTA.setHostname("clock"); // upload with clock.local
+    ArduinoOTA
+        .onStart([]()
+                 {
+      String type;
+      if (ArduinoOTA.getCommand() == U_FLASH) {
+        type = "sketch";
+      } else {  // U_SPIFFS
+        type = "filesystem";
+      }
+
+      // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+      Serial.println("Start updating " + type); })
+        .onEnd([]()
+               { Serial.println("\nEnd"); })
+        .onProgress([](unsigned int progress, unsigned int total)
+                    { Serial.printf("Progress: %u%%\r", (progress / (total / 100))); })
+        .onError([](ota_error_t error)
+                 {
+      Serial.printf("Error[%u]: ", error);
+      if (error == OTA_AUTH_ERROR) {
+        Serial.println("Auth Failed");
+      } else if (error == OTA_BEGIN_ERROR) {
+        Serial.println("Begin Failed");
+      } else if (error == OTA_CONNECT_ERROR) {
+        Serial.println("Connect Failed");
+      } else if (error == OTA_RECEIVE_ERROR) {
+        Serial.println("Receive Failed");
+      } else if (error == OTA_END_ERROR) {
+        Serial.println("End Failed");
+      } });
+    ArduinoOTA.begin();
+
+    Serial.println("Maj OTA configuré");
+    Serial.print("IP address: ");
+    Serial.println(WiFi.localIP());
+
     configTime(3600 * 2, 1, "pool.ntp.org");
 
     getLocalTime((tm *)&time_clock, 10000);
     Serial.println("Temps récupérer");
+
     /*Initilisation du Timer pour l'horloge*/
     hw_timer_t *timer_clock = NULL;
     timer_clock = timerBegin(0, 80, true);                    // Initialise le timer 0, diviseur 80 (donc fréquence de 1MHz), comptage ascendant
@@ -65,4 +105,6 @@ void loop()
         ui.refresh(); // Rafraichir l'écran
         clock_update = false;
     }
+
+    ArduinoOTA.handle(); // OTA
 }
